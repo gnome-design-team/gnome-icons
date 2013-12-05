28a80a54ba65f303274c82dcd5d6ebfa28857a05
Jakub Steiner <jimmac@gmail.com>
2013-12-05 14:41:45 +0100

    / ‘apps/hicolor/16x16/apps/photos.png’ / ‘apps/hicolor/24x24/apps/photos.png’ / ‘apps/render-bitmaps.rb’

--- apps/render-bitmaps.rb
+++ apps/render-bitmaps.rb
@@ -21,7 +21,7 @@ def renderit(file,explicit)
         FileUtils.makedirs(dir) unless File.exists?(dir)
         if (!explicit && File.exists?(out))
           print "-" #skip if PNG exists
-        elseif (box.attributes['width']==256)
+        elsif (box.attributes['width']==256)
           system(cmd)
           print "."
           dir = "hicolor/512x512/#{context}"
