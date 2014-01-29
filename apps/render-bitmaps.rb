317db858020d46cb9c1a0d0a63e8aa839bd1bf46
Sam Hewitt <hewittsamuel@gmail.com>
2014-01-28 22:18:42 -0500

    / ‘apps/render-bitmaps.rb’

--- apps/render-bitmaps.rb
+++ apps/render-bitmaps.rb
@@ -3,9 +3,8 @@
 require "rexml/document"
 require "fileutils"
 include REXML
-
 INKSCAPE = '/usr/bin/inkscape'
-SOURCES = ('src/apps', 'src/categories', 'src/devices', 'src/games', 'src/mimetypes', 'src/places', 'src/preferences', 'src/web')
+SRC = "./src"
 
 def renderit(file,explicit)
   svg = Document.new(File.new("#{SRC}/#{file}", 'r'))
@@ -16,7 +15,7 @@ def renderit(file,explicit)
       icon_name = icon.elements["text[@inkscape:label='icon-name']/tspan"].nil? ? 'blank' : icon.elements["text[@inkscape:label='icon-name']/tspan"].text
       puts "#{file}:#{icon.attributes['inkscape:label']}  #{context}/#{icon_name}"
       icon.each_element("rect") do |box|
-        dir = "Moka/#{box.attributes['width']}x#{box.attributes['height']}/#{context}"
+        dir = "hicolor/#{box.attributes['width']}x#{box.attributes['height']}/#{context}"
         out = "#{dir}/#{icon_name.gsub(/$/,'.png')}"
         cmd = "#{INKSCAPE} -i #{box.attributes['id']} -e #{out} #{SRC}/#{file} > /dev/null 2>&1"
         FileUtils.makedirs(dir) unless File.exists?(dir)
@@ -25,7 +24,7 @@ def renderit(file,explicit)
         elsif (box.attributes['width']=="256")
           system(cmd)
           print "."
-          dir = "Moka/512x512/#{context}"
+          dir = "hicolor/512x512/#{context}"
           out = "#{dir}/#{icon_name.gsub(/$/,'.png')}"
           cmd = "#{INKSCAPE} -i #{box.attributes['id']} -d 180 -e #{out} #{SRC}/#{file} > /dev/null 2>&1"
           system(cmd)
@@ -40,22 +39,19 @@ def renderit(file,explicit)
   end
 end
 
-for source in SOURCES:
-
-  SRC = os.path.join('.', source)
-  if (ARGV[0].nil?) #render all SVGs
-    system("mkdir Moka/") unless File.exists?('Moka')
-    puts "Rendering from SVGs in #{SRC}"
-    Dir.foreach(SRC) do |file|
-      renderit(file, false) if file.match(/svg$/)
-    end
-    puts "\nrendered all SVGs"
-  else #only render the SVG passed
-    file = "#{ARGV[0]}.svg"
-    if (File.exists?("#{SRC}/#{file}"))
-      renderit(file, true)
-      puts "\nrendered #{file}"
-    else
-      puts "[E] No such file (#{file})"
-    end
+if (ARGV[0].nil?) #render all SVGs
+  system("mkdir hicolor/") unless File.exists?('hicolor')
+  puts "Rendering from SVGs in #{SRC}"
+  Dir.foreach(SRC) do |file|
+    renderit(file, false) if file.match(/svg$/)
   end
+  puts "\nrendered all SVGs"
+else #only render the SVG passed
+  file = "#{ARGV[0]}.svg"
+  if (File.exists?("#{SRC}/#{file}"))
+    renderit(file, true)
+    puts "\nrendered #{file}"
+  else
+    puts "[E] No such file (#{file})"
+  end
+end
