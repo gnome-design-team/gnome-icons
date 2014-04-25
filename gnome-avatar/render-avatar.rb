dd06279929fa4e5de083a8b75e5b09efea2d9122
Jakub Steiner <jimmac@gmail.com>
2014-04-26 00:52:28 +0200

    / ‘gnome-avatar/avatar.png’ / ‘gnome-avatar/out.svg’ / ‘gnome-avatar/render-avatar.rb’

--- gnome-avatar/render-avatar.rb
+++ gnome-avatar/render-avatar.rb
@@ -12,6 +12,9 @@ def render(image)
   templatexml = Document.new(File.new(TEMPLATE,'r'))
   #puts templatexml
   templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['xlink:href'] = image
+  templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['sodipodi:absref'] = 
+    "file://#{ABSPREFIX}/#{image}"
+  puts templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['sodipodi:absref']
   tmp_f = File.new(TMP,'w+')
   tmp_f.puts templatexml
   tmp_f.close
