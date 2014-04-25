70781cff08155536226661a5ec446ce2fef29747
Jakub Steiner <jimmac@gmail.com>
2014-04-26 00:23:08 +0200

    / ‘gnome-avatar/render-avatar.rb’

--- gnome-avatar/render-avatar.rb
+++ gnome-avatar/render-avatar.rb
@@ -2,6 +2,18 @@
 require "rexml/document"
 include REXML
 
+TEMPLATE = avatar.svg
+
+if ARGV[0]
+  render(ARGV[0])
+else
+  puts "Need a bitmap parameter"
+end
+
+def render(image)
+  puts image
+end
+
 #template.root.elements["//pattern[@inkscape:label='avatar']/image"].attributes['xlink:href'] = 
 #  ABSPREFIX + self.background
 #template.root.elements["//pattern[@inkscape:label='avatar']/image"].attributes['sodipodi:absref'] = 
