26a6aa121559b7c3bb89a1d41a82bab0ab5e6f70
Jakub Steiner <jimmac@gmail.com>
2014-04-26 00:18:37 +0200

    / ‘gnome-avatar/render-avatar.rb’

---
+++ gnome-avatar/render-avatar.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require "rexml/document"
+include REXML
+
+#template.root.elements["//pattern[@inkscape:label='background']/image"].attributes['xlink:href'] = 
+#  ABSPREFIX + self.background
+#template.root.elements["//pattern[@inkscape:label='background']/image"].attributes['sodipodi:absref'] = 
+#  ABSPREFIX + self.background
+#template.root.elements["//image[@inkscape:label='logo']"].attributes['xlink:href'] = LOGO
+#template.root.elements["//image[@inkscape:label='logo']"].attributes['sodipodi:absref'] = LOGO
