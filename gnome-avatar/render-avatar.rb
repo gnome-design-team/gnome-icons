9b7758cd1ed3970d19b3f6e5152402e779d29270
Jakub Steiner <jimmac@gmail.com>
2014-04-26 00:51:58 +0200

    / ‘gnome-avatar/render-avatar.rb’

--- gnome-avatar/render-avatar.rb
+++ gnome-avatar/render-avatar.rb
@@ -6,14 +6,15 @@ INKSCAPE = "/usr/bin/inkscape"
 TEMPLATE = "avatar.svg"
 OUT = "avatar.png"
 ABSPREFIX = Dir.pwd
+TMP = "out.svg"
 
 def render(image)
   templatexml = Document.new(File.new(TEMPLATE,'r'))
   #puts templatexml
   templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['xlink:href'] = image
-  templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['sodipodi:absref'] = 
-    "file://#{ABSPREFIX}/#{image}"
-  puts templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['sodipodi:absref']
+  tmp_f = File.new(TMP,'w+')
+  tmp_f.puts templatexml
+  tmp_f.close
   exec = "#{INKSCAPE} -e #{OUT} #{TEMPLATE}"
   system(exec)
 end
