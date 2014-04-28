71d8003d46ef6e236304b8b57140994a84660ff8
Jakub Steiner <jimmac@gmail.com>
2014-04-28 20:18:21 +0200

    / ‘gnome-avatar/render-avatar.py’

--- gnome-avatar/render-avatar.py
+++ gnome-avatar/render-avatar.py
@@ -15,12 +15,12 @@ def render(image):
   #templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['sodipodi:absref'] = 
   #  "file://#{ABSPREFIX}/#{image}"
   #puts templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['sodipodi:absref']
-  tmp_f = File.new(TMP,'w+')
+  #tmp_f = File.new(TMP,'w+')
   #tmp_f.puts templatexml
-  tmp_f.close
+  #tmp_f.close
   exec = "#{INKSCAPE} -e #{OUT} #{TMP}"
-  system(exec)
-  File.unlink(TMP)
+  os.system(exec)
+  #File.unlink(TMP)
 
 
 if sys.argv[0]:
