e5f37f78e2e083bb49286acf551a56d68fdbe608
Jakub Steiner <jimmac@gmail.com>
2014-04-28 20:41:10 +0200

    / ‘gnome-avatar/render-avatar.py’

--- gnome-avatar/render-avatar.py
+++ gnome-avatar/render-avatar.py
@@ -13,7 +13,7 @@ TMP = "out.svg"
 
 def render(image):
   templatexml = lxml.etree.parse(TEMPLATE)
-  print(templatexml)
+  print(templatexml.root)
   #templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['xlink:href'] = image
   #templatexml.root.elements["//pattern[@inkscape:label='avatarpattern']/image"].attributes['sodipodi:absref'] = 
   #  "file://#{ABSPREFIX}/#{image}"
