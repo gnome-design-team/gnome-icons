741c7ec7253a481bcc248b43fa1777e58a8967e5
Jakub Steiner <jimmac@gmail.com>
2014-05-05 17:36:14 +0200

    gnome theme is adwaita now

--- apps-symbolic/r.rb
+++ apps-symbolic/r.rb
@@ -8,7 +8,7 @@ include REXML
 INKSCAPE = '/usr/bin/inkscape'
 #INKSCAPE = '/usr/bin/inkscape' # like this works for me, while using `which` inkscape hangs
 SRC = "src/apps.svg"
-PREFIX = "gnome/scalable"
+PREFIX = "adwaita/scalable"
 
 def chopSVG(icon)
 	FileUtils.mkdir_p(icon[:dir]) unless File.exists?(icon[:dir])
